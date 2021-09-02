0cd477fdc4bc61bf922567687d518318c3f23f56
Daniel <41334756+BalyberdinDaniil@users.noreply.github.com>
2021-09-02 15:30:14 +0500

    Update update_control.ino

--- update_control.ino
+++ update_control.ino
@@ -1,8 +1,7 @@
-int readAnalogInput(uint8_t input, uint8_t to) {
+int readAnalogInput(uint8_t input, uint8_t maxValue) {
   int sensor_value = mozziAnalogRead(input); // value is 0-1023
-  return  map(sensor_value, 0, 1023, 0, to);
+  return  map(sensor_value, 0, 1023, 0, maxValue);
 }
-
 
 void updateControl() {
   freq = readAnalogInput(2, 254) + 1;
