e75a268cd204e5c85a35895f6d4ae1fcdd23e559
Daniel <41334756+BalyberdinDaniil@users.noreply.github.com>
2024-05-03 12:26:06 +0500

    Update oscillator.ino
    интерполировал потенциометр

--- oscillator.ino
+++ oscillator.ino
@@ -1,18 +1,23 @@
 int updateAudio() {
-  int result;
-  aTri.setFreq(freq >> 4);
-  aSaw.setFreq(freq >> 4);
-  aSqrt.setFreq(freq >> 4);
-  aSawRev.setFreq(freq >> 4);
 
-  if (wave < 64)
-    result = (int)aTri.next();
-  else if (wave < 128)
+  int result;
+
+  if (wave <= 255 && wave > 237)
     result = (int)aSaw.next();
-  else if (wave < 192)
+  if (wave <= 237 && wave > 223)
     result = (int)aSqrt.next();
-  else
+  if (wave <= 223 && wave > 159)
     result = (int)aSawRev.next();
+  if (wave <= 150 && wave >= 0)
+    result = (int)aTri.next();
 
   return (result * depth) >> 8;
 }
+/*
+Интерполяция потенциометра
+  255
+  237
+  223
+  150
+  0
+*/
