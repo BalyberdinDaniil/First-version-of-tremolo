72b411eebada8e549a7de1d3aa053388ca9b8d5c
Daniel <41334756+BalyberdinDaniil@users.noreply.github.com>
2024-05-03 12:25:11 +0500

    Update First-version-of-tremolo-main.ino

--- First-version-of-tremolo-main.ino
+++ First-version-of-tremolo-main.ino
@@ -5,12 +5,17 @@
 #include <tables/saw2048_int8.h>
 Oscil <SAW2048_NUM_CELLS, AUDIO_RATE> aSaw(SAW2048_DATA);
 
+//up - down
 #include <tables/triangle2048_int8.h>
 Oscil <TRIANGLE2048_NUM_CELLS, AUDIO_RATE> aTri(TRIANGLE2048_DATA);
+
+#include <tables/envelop2048_uint8.h>
+Oscil <ENVELOP2048_NUM_CELLS, AUDIO_RATE> aSin(ENVELOP2048_DATA);
 
 #include <tables/square_no_alias_2048_int8.h>
 Oscil <SQUARE_NO_ALIAS_2048_NUM_CELLS, AUDIO_RATE> aSqrt(SQUARE_NO_ALIAS_2048_DATA);
 
+//down - up
 #include <tables/sawRevers2048_int8.h>
 Oscil <SAWREV2048_NUM_CELLS, AUDIO_RATE> aSawRev(SAWREV2048_DATA);
 
@@ -18,11 +23,20 @@ Oscil <SAWREV2048_NUM_CELLS, AUDIO_RATE> aSawRev(SAWREV2048_DATA);
 #define CONTROL_RATE 64 // Hz, powers of 2 are most reliable
 
 uint8_t freq = 1;
-uint8_t depth = 1;
+uint8_t depth = 0;
 uint8_t wave = 0;
 
+bool buttonWasUp = true;  // была ли кнопка отпущена?
+bool tremEnabled = false;  // включено ли тремоло?
+uint32_t btnTimer = 0; // таймер для заммены delay()
+
 void setup() {
+  pinMode(3, INPUT_PULLUP);
+
   startMozzi(CONTROL_RATE);
+  freq = readAnalogInput(2, 127) + 20;
+  wave = readAnalogInput(0, 3);
+
   aTri.setFreq(freq);
   aSaw.setFreq(freq);
   aSqrt.setFreq(freq);
